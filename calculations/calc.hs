2ff0adc971dbc872d6e2f43a7fb9b3d285619a4e
MaXwell Falstein <max@saxe-falstein.co.uk>
2015-11-10 13:16:13 +0000

    Update calc.hs

--- calculations/calc.hs
+++ calculations/calc.hs
@@ -1,1 +1,10 @@
-// I need to get the file from my SAN
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+minimsd :: Int -> Int
+minimsd 0 = 1
+minimsd n = ( ( n * ( factorial ( n - 1 ) ) ) ^ 36 ) // Broken in ghci due to stack limit
+
+mmsdtail :: Int -> Int
+mmsdtail 
