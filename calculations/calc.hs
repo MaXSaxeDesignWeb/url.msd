31fe77f05544df653ea44d2c52884743f0d01cb5
MaXwell Falstein <MaXwellFalstein@users.noreply.github.com>
2016-03-25 23:10:12 +0000

    Update calc.hs

--- calculations/calc.hs
+++ calculations/calc.hs
@@ -2,9 +2,11 @@ factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
 
+-- This function breaks GHCi--
 minimsd :: Int -> Int
 minimsd 0 = 1
 minimsd n = ( ( n * ( factorial ( n - 1 ) ) ) ^ 36 ) // Broken in ghci due to stack limit
 
+-- This function should not break GHCi --
 mmsdtail :: Int -> Int
-mmsdtail 
+mmsdtail n = 
